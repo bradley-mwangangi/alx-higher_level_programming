09cee791f90202e8817c68b678cfdca43d481dc6
Bradley Mwangangi <146893969+bradley-mwangangi@users.noreply.github.com>
2023-10-09 14:45:10 +0300

    code rewrite
    return 0 for empty tuple or missing values

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,11 +2,9 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = ()
 
-    max_len = max(len(tuple_a), len(tuple_b))
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
 
-    for i in range(max_len):
-        a = tuple_a[i] if i < len(tuple_a) else 0
-        b = tuple_b[i] if i < len(tuple_b) else 0
-        new_tuple += (a + b,)
+    new_tuple = (a[0] + b[0], a[1] + b[1])
 
     return new_tuple
